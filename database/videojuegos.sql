c05de67d9953bf9d615cc0df9886b90568471e53
Tomás Tourn <tomytourn1@gmail.com>
2022-10-13 23:09:32 -0300

    sunbo db

--- database/videojuegos.sql
+++ database/videojuegos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 11, 2022 at 06:50 PM
+-- Generation Time: Oct 14, 2022 at 04:08 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -39,10 +39,9 @@ CREATE TABLE `genero` (
 
 INSERT INTO `genero` (`id_genero`, `genero`, `descripcion_genero`) VALUES
 (1, 'Acción', 'Generalmente implican avanzar a través de niveles, eliminando hordas de enemigos y resolviendo problemas. Muchos videojuegos incluyen uno o más.'),
-(2, 'Aventura', 'Caracterizados por la investigación, exploración, la solución de rompecabezas, la interacción con personajes del videojuego, y un enfoque en el relato en vez de desafíos basados en reflejos . Es importante observar que este término no tiene relación con las películas y novelas de aventura y no es indicativo del tema o del sujeto que trata. '),
 (3, 'Carreras', 'Un videojuego de carreras es un videojuego en el que se imitan competencias entre vehículos. Usualmente el objetivo es recorrer cierta distancia o ir de un sitio hacia otro en el menor tiempo posible, como en el automovilismo y el motociclismo'),
 (4, 'Disparos', 'permiten controlar un personaje que, por norma general, dispone de un arma y por lo general son multiplayer,Si  hace uso de internet, se puede catalogar en una serie de divisiones: Los juegos en equipo y los juegos individuales'),
-(5, 'Estrategia', 'requieren que el jugador ponga en práctica sus habilidades de planeamiento y pensamiento para maniobrar, gestionando recursos de diverso tipo (materiales, humanos, militares...) para conseguir la victoria'),
+(5, 'Estrategia', 'requieren que el jugador ponga en práctica sus habilidades de planeamiento y pensamiento para maniobrar, gestionando recursos de diverso tipo (materiales, humanos, militares....) para conseguir la victoria'),
 (6, 'Indie', 'desarrollados por grupos reducidos de individuos o pequeñas empresas. No suelen contar con el apoyo financiero de distribuidores y publicistas, sino que su difusión depende principalmente de la voluntad de los jugadores para compartirlos con otros.');
 
 -- --------------------------------------------------------
@@ -65,10 +64,13 @@ CREATE TABLE `juego` (
 --
 
 INSERT INTO `juego` (`id_juego`, `id_genero_fk`, `nombre`, `precio`, `descripcion`, `imagen`) VALUES
-(2, 1, 'Grand Theft Auto V', 599, 'mundo abierto con historia, disparos, autos y lo que quieras.', 'images/gtaV.jpg'),
-(3, 3, 'F1 2022', 700, 'Ponte al volante en la nueva temporada: coches rediseñados y nuevas reglas que definen las carreras, con los pilotos de 2022.', 'images/f1.jpg'),
-(4, 6, 'cult of the lamb', 1200, 'Crea tu propia secta en una tierra de falsos profetas, aventurándote en regiones diversas y misteriosas para construir una leal comunidad de adeptos de los bosques, y difunde tu Palabra para convertir tu secta en la única y verdadera.', 'images/cult.jpg'),
-(5, 3, 'forza horizon 5', 10000, 'Forza Horizon 5 es un videojuego de carreras multijugador en línea desarrollado por Playground Games y publicado por Xbox Game Studios. Es el quinto título de Forza Horizon y la duodécima entrega principal de la serie Forza. El juego está ambientado en una representación ficticia de México.', 'images/forza5.jpg');
+(24, 1, 'Ark Survival Evolved', 300, 'doma dinosaurios y sobrevive en una peligrosa isla', 'images/6348549b324a8.jpg'),
+(27, 1, 'Grand Theft Auto V', 500, 'mundo abierto con historia, disparos, autos y lo que quieras.', 'images/6348592084a96.jpg'),
+(28, 3, 'F1 2022', 700, 'Ponte al volante en la nueva temporada: coches rediseñados y nuevas reglas que definen las carreras, con los pilotos de 2022.', 'images/6348594e867c4.jpg'),
+(29, 6, 'cult of the lamb', 1200, 'Crea tu propia secta en una tierra de falsos profetas, aventurándote en regiones diversas y misteriosas para construir una leal comunidad de adeptos de los bosques, y difunde tu Palabra para convertir tu secta en la única y verdadera.', 'images/63485980bc7aa.jpg'),
+(30, 3, 'Forza Horizon 5', 1000, 'forza horizon 5', 'images/634859a32b37a.jpg'),
+(31, 1, 'Assasins Creed Origins', 600, 'Ambientado en el misterioso Antiguo Egipto, Assassin’s Creed® Origins es un nuevo comienzo. Experimenta una nueva forma de pelear mientras exploras las majestuosas pirámides y las tumbas secretas a través del territorio del Antiguo Egipto y encuentra inolvidables historias a lo largo de tu travesía. Y descubre la historia del origen de la Hermandad de los Asesinos.', 'images/63485a22e1abc.jpg'),
+(32, 1, 'Shadow of the Tomb Raider', 7006, 'En Shadow of the Tomb Raider: Definitive Edition, vive el capítulo final de la historia sobre el origen de Lara, en el que se convierte en la saqueadora de tumbas que está destinada a ser.', 'images/63485a8f5765d.jpg');
 
 -- --------------------------------------------------------
 
@@ -119,13 +121,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `genero`
 --
 ALTER TABLE `genero`
-  MODIFY `id_genero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_genero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `juego`
 --
 ALTER TABLE `juego`
-  MODIFY `id_juego` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_juego` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- Constraints for dumped tables
